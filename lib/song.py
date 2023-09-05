2e850c84027fb9ee704fab0772d00c1d5f6222e6
+ <lucy.njoroge1@student.moringaschool.com>
2023-09-05 22:01:47 +0300

    complete

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,26 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create(cls, name, album):
+        song = cls(name, album)  # Create a new Song instance
+        song.save()  # Save the new instance to the database
+        return song  # Return the created instance
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.album))
+# Example usage:
+new_song = Song.create("New Song", "New Album")
+print(new_song.name)  # Should print "New Song"
+print(new_song.album)  # Should print "New Album"
